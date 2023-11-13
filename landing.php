e9c3c001779eab36e1178612f616bafdfec7bdfc
mysite523 <78149454+mysite523@users.noreply.github.com>
2023-11-13 12:27:13 +0300

    landing.php

--- landing.php
+++ landing.php
@@ -17,45 +17,36 @@ $signedHash = hash_hmac('sha1', $signature['timestamp'], LANDING_SECRET_KEY);
 if ($signedHash !== $signature['hash'] || strtotime(SIGNATURE_TTL, $signature['timestamp']) < time()) {
     exit('Access denied');
 }
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html>
 
 <head>
-    <title>My Landing Page</title>
+    <title>BeMob Landing Signature Example</title>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    
-    <style>
-.btn {
-  border: 2px solid blue;
-  background-color: dodgerblue;
-  color: white;
-  padding: 14px 28px;
-  font-size: 16px;
-  cursor: pointer;
-}
-.success {
-  border-color: #2196F3;
-  color: white;
-    }
-</style>
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
 </head>
-
 <body>
-     <div class="container theme-showcase" role="main">
-        <div class="jumbotron">
-            <h1>My Landing Page</h1>
-            <p>Special offer for my users!</p>
-<p>
-                <a href="https://8vlr0.bemobtrk.com/click" class="btn success">https://8vlr0.bemobtrk.com/click</a>
-            </p>
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">BeMob Landing Signature Example</a>
         </div>
     </div>
-</body>
+</nav>
 
+<div class="container theme-showcase" role="main">
+    <div class="jumbotron">
+        <h1>BeMob Landing Signature Example</h1>
+        <p>
+            <a href="https://8vlr0.bemobtrk.com/click" id="ctaUrl" class="btn btn-success">Click me!</a>
+        </p>
+    </div>
+</div>
+
+</body>
 </html>
